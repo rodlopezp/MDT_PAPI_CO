0273771de47f5e43ce392c50998de2edd9742bc6
rodlopezp <jorito1@gmail.com>
2017-07-25 17:07:41 -0400

    Se implementa un algoritmo CO recursivo para la transposición de matrices.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,20 @@
 #include "matrix_functions.h"
 #include "papi_utils.h"
 
+const int DEBUG = 0;
+
 int main ()
 {
     PAPI_init();
     PAPI_counterCheck();
 
-    matrix<float> test; const unsigned int matDim = 1024 * 4;
-    alloc_matrix(&test, matDim, matDim);
-    randFloat_init_matrix(&test, 1.0);
-    //print_matrix(&test);
+    matrix<int> test, outTest, outTest2;
+    const unsigned int matDim = 1024;
+    alloc_matrix(&test, matDim, matDim); alloc_matrix(&outTest, matDim, matDim);
+    alloc_matrix(&outTest2, matDim, matDim);
+    randInt_init_matrix(&test, 10);
+
+    if (DEBUG == 1) { print_matrix(&test); }
 
     int events[1] = {PAPI_L1_DCM};
     long long values[1];
@@ -21,18 +26,36 @@ int main ()
     float t0 = PAPI_getCPUTime(); //Timestamp inicial.
     PAPI_startCounters(events, 1);
 
-    matrix_transpose_naive(&test);
-
-    //PAPI_stop_counters();
+    matrix_transpose_naive(&test, &outTest, test.rows, test.cols, 0, 0);
 
     PAPI_readCounters(values, 1);
     float t1 = PAPI_getCPUTime();  //Timestamp final.
 
-    dealloc_matrix(&test);
+    printf("Time elapsed = %fs\n", (t1-t0));
+    printf("L1 data cache misses = %lld\n", values[0]);
+
+    if (DEBUG == 1) { print_matrix(&outTest); }
+
+    //TODO: Refactorizar en función.
+    if (int retVal = PAPI_stop_counters(values, 1) != PAPI_OK){
+        fprintf(stderr, "PAPI failed to stop counters: %s\n", PAPI_strerror(retVal));
+        exit(1);
+    }
+
+    t0 = PAPI_getCPUTime();
+    PAPI_startCounters(events, 1);
+
+    matrix_transpose_co(&outTest, &outTest2, test.rows, test.cols, 0, 0);
+
+    PAPI_readCounters(values, 1);
+    t1 = PAPI_getCPUTime();  //Timestamp final.
 
     printf("Time elapsed = %fs\n", (t1-t0));
-    //printf("Total FLOPS = %lld\n",values[0]);
     printf("L1 data cache misses = %lld\n", values[0]);
+
+    if (DEBUG == 1) { print_matrix(&test); }
+
+    dealloc_matrix(&test);
 
     return 0;
 }
--- matrix_functions.h
+++ matrix_functions.h
@@ -18,16 +18,17 @@ struct matrix{
 };
 
 /**
- * Una implementación naive de transposición de matrices, in-place, para matrices cuadradas.
+ * Una implementación naive de transposición de matrices cuadradas.
  **/
 template <typename T>
-void matrix_transpose_naive(matrix<T>* matrix){
-    T tmp;
-    for(unsigned int i = 0; i < matrix->rows; i++){
-        for(unsigned int j = i; j < matrix->cols; j++){
-            tmp = matrix->elements[i * matrix->rows + j];
-            matrix->elements[i * matrix->rows + j] = matrix->elements[j * matrix->cols + i];
-            matrix->elements[j * matrix->cols + i] = tmp;
+void matrix_transpose_naive(matrix<T>* inMatrix, matrix<T>* outMatrix,
+                            int rows, int cols,
+                            unsigned int rowOffset, unsigned int colOffset){
+    const unsigned int rowLimit = rowOffset + rows;
+    const unsigned int colLimit = colOffset + cols;
+    for(unsigned int i = rowOffset; i < rowLimit; i++){
+        for(unsigned int j = colOffset; j < colLimit; j++){
+            outMatrix->elements[j * rows + i] = inMatrix->elements[i * rows + j];
         }
     }
 }
@@ -48,11 +49,11 @@ void print_matrix(matrix<T>* matrix){
 }
 
 /**
- *  Reserva espacio para un objeto int_matrix, mediante malloc.
+ *  Reserva espacio para un objeto T_matrix, mediante malloc.
  **/
 template <typename T>
 void alloc_matrix(matrix<T>* matrix, unsigned int rows, unsigned int cols){
-    matrix->elements = (T*) malloc(sizeof(int) * rows * cols);
+    matrix->elements = (T*) malloc(sizeof(T) * rows * cols);
     matrix->rows = rows;
     matrix->cols = cols;
 }
@@ -68,6 +69,7 @@ void dealloc_matrix(matrix<T>* matrix){
 /**
  *  Inicializa la matriz con valores enteros aleatorios entre cero y upper_limit (excluyendo este
  *  último).
+ *  TODO: Cambiar el rng por uno que use la STL.
  **/
 template <typename T>
 void randInt_init_matrix(matrix<T>* matrix, unsigned int upper_limit){
@@ -80,12 +82,38 @@ void randInt_init_matrix(matrix<T>* matrix, unsigned int upper_limit){
 /**
  *  Inicializa la matriz con valores flotantes aleatorios entre cero y upper_limit (excluyendo este
  *  último).
+ *  TODO: Cambiar el rng por uno que use la STL.
  **/
 template <typename T>
 void randFloat_init_matrix(matrix<T>* matrix, float upper_limit){
     srand(time(NULL));
     for(unsigned int i = 0; i < matrix->rows * matrix->cols; i++){
         matrix->elements[i] = static_cast<float>(rand())/static_cast<float>(RAND_MAX / upper_limit);
+    }
+}
+
+/**
+ *  Algoritmo recursivo que divide el problema hasta un límite (arbitrario), que luego invoca a
+ *  matrix_transpose_naive.
+**/
+const int minSubMatrixSize = 4; //Tamaño mínimo de la sub-matriz.
+template <typename T>
+void matrix_transpose_co(matrix<T>* inMatrix, matrix<T>* outMatrix,
+                         int rows, int cols, int rowOffset, int colOffset){
+    if( rows > minSubMatrixSize || cols > minSubMatrixSize){
+        if( cols >= rows){
+            const int halfCols = cols / 2;
+            matrix_transpose_co(inMatrix, outMatrix, rows, halfCols, rowOffset, colOffset);
+            matrix_transpose_co(inMatrix, outMatrix, rows, halfCols, rowOffset, colOffset + halfCols);
+        }
+        else{
+            const int halfRows = rows / 2;
+            matrix_transpose_co(inMatrix, outMatrix, halfRows, cols, rowOffset, colOffset);
+            matrix_transpose_co(inMatrix, outMatrix, halfRows, cols, rowOffset + halfRows, colOffset);
+        }
+    }
+    else{
+        matrix_transpose_naive(inMatrix, outMatrix, rows, cols, rowOffset, colOffset);
     }
 }
 
