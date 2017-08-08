504a33d5de90d586980bbb033199b76b1d00db64
rodlopezp <jorito1@gmail.com>
2017-08-08 18:44:01 -0400

    Refactorización de funciones para pruebas.
    Se cambió la forma en que se guardan los datos de las pruebas.

--- main.cpp
+++ main.cpp
@@ -2,20 +2,13 @@
  * Main entry point para uso de PAPI.
  */
 
-#include <algorithm>
-
 #include "matrix_functions.h"
 #include "papi_utils.h"
 #include "matrix_tests.h"
 
+#include <algorithm>
 
-enum DEBUG_MODE {
-    NODEBUG =   0,
-    PRINT   =   1,
-    ASSERT  =   2,
-};
 
-DEBUG_MODE DEBUG = ASSERT;
 
 int main ()
 {
@@ -23,13 +16,12 @@ int main ()
     PAPI_counterCheck();
 
     unsigned int half_initial_size = 8;
-    std::vector<unsigned int> matrix_sizes(5);
-    std::string test_name = "test_co";
+    std::vector<unsigned int> matrix_sizes(8);
     std::generate(matrix_sizes.begin(), matrix_sizes.end(),
                   [&]{return half_initial_size *= 2;});
 
-    run_matrix_algo_test(matrix_transpose_naive<float>, 100, matrix_sizes, "test_naive");
-    run_matrix_algo_test(matrix_transpose_co<float>, 100, matrix_sizes, test_name);
+    run_matrix_algo_test(matrix_transpose_naive<float>, 100, matrix_sizes, "naive");
+    run_matrix_algo_test(matrix_transpose_co<float>, 100, matrix_sizes, "co");
 
     return 0;
 }
--- matrix_tests.h
+++ matrix_tests.h
@@ -3,6 +3,7 @@
 
 #include "matrix_functions.h"
 #include "papi_utils.h"
+
 #include <vector>
 #include <assert.h>
 #include <iostream>
@@ -41,28 +42,23 @@ void run_matrix_algo_iter(matrix_transform<T> matTrans, unsigned int iter, matri
                           matrix<T>* matOut, std::vector<long long>& cache_misses_L1,
                           std::vector<long long>& cache_misses_L2, std::vector<float>& time_spent){
     float test_start, test_end, test_time;
-
     const int numOfEvents = 2;
     int events[numOfEvents] = {PAPI_L1_TCM, PAPI_L2_TCM};
     long long int values[numOfEvents];
 
     test_start = PAPI_getCPUTime();
     PAPI_startCounters(events, numOfEvents);
+
     matTrans(matIn, matOut, matIn->rows, matIn->cols, 0, 0);
-    PAPI_readCounters(values, numOfEvents);
-    test_end = PAPI_getCPUTime();
-    test_time = test_end - test_start;
 
     PAPI_stopCounters(values, numOfEvents);
+    test_end = PAPI_getCPUTime();
+
+    test_time = test_end - test_start;
 
     cache_misses_L1.push_back(values[0]);
     cache_misses_L2.push_back(values[1]);
     time_spent.push_back(test_time);
-
-//    testResults << "Iteración " << iter << " :\n";
-//    testResults << "Tiempo transcurrido: " << (test_time) << "\n";
-//    testResults << "L1 total cache misses = " << values[0] << "\n";
-//    testResults << "L2 total cache misses = " << values[1] << "\n";
 }
 
 /**
@@ -75,15 +71,22 @@ void run_matrix_algo_test(matrix_transform<T> matTrans, unsigned int numIter,
                           std::vector<unsigned int> matrixSizes, std::string testName){
     std::ofstream testResults = open_test_file(testName);
     matrix<T> matIn, matOut;
-    std::vector<long long int> cache_misses_L1, cache_misses_L2;
-    std::vector<float> time_spent;
-    float avg_L1_misses, avg_L2_misses, avg_time_spent;
+    std::vector<long long int> cache_misses_L1(numIter), cache_misses_L2(numIter);
+    std::vector<float> time_spent(numIter);
+    std::size_t avg_L1_misses, avg_L2_misses;
+    double avg_time_spent;
+    std::cout << "Ejecutando prueba " << testName << "...\n";
 
     for(auto matrixSize : matrixSizes){
+        cache_misses_L1.clear();
+        cache_misses_L2.clear();
+        time_spent.clear();
+
         testResults << "Tamaño de matriz: " << matrixSize << "\n";
         alloc_matrix(&matIn, matrixSize, matrixSize);
         alloc_matrix(&matOut, matrixSize, matrixSize);
         randFloat_init_matrix(&matIn, 10.0);
+        std::cout << "Ejecutando prueba para matriz " << matrixSize << 'x' << matrixSize << "...\n";
 
         for(unsigned int iter = 0; iter < numIter; iter++){
             run_matrix_algo_iter(matTrans,iter, &matIn, &matOut, cache_misses_L1, cache_misses_L2,
@@ -93,19 +96,26 @@ void run_matrix_algo_test(matrix_transform<T> matTrans, unsigned int numIter,
         dealloc_matrix(&matIn);
         dealloc_matrix(&matOut);
 
-        avg_L1_misses = std::accumulate(cache_misses_L1.begin(), cache_misses_L1.end(), 0.0);
+        avg_L1_misses = std::accumulate(cache_misses_L1.begin(), cache_misses_L1.end(), 0LL);
         avg_L1_misses = avg_L1_misses / cache_misses_L1.size();
 
-        avg_L2_misses = std::accumulate(cache_misses_L2.begin(), cache_misses_L2.end(), 0.0);
+        avg_L2_misses = std::accumulate(cache_misses_L2.begin(), cache_misses_L2.end(), 0LL);
         avg_L2_misses = avg_L2_misses / cache_misses_L1.size();
 
-        avg_time_spent = std::accumulate(time_spent.begin(), time_spent.end(), 0.0);
+        avg_time_spent = std::accumulate(time_spent.begin(), time_spent.end(), 0.0f);
         avg_time_spent = avg_L2_misses / time_spent.size();
+
+        testResults << "Tiempo de ejecución promedio: " << avg_time_spent << "s\n";
+        testResults << "Promedio de fallas de cache L1 totales: " << avg_L1_misses << "\n";
+        testResults << "Promedio de fallas de cache L2 totales: " << avg_L2_misses << "\n";
+
+        if(false){
+            testResults << "DEBUG INFO:\n";
+            for(auto value : time_spent){
+                testResults << value << '\n';
+            }
+        }
     }
-
-
-
-
 }
 
 #endif // MATRIX_TESTS_H_INCLUDED
